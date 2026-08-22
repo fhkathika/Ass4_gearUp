45a4f7a6ed9b30fb88205f89bedbd9334566dba2
fhkathika <fhkathika057@gmail>
2026-08-22 11:18:47 +0600

    added get all bookings api

--- src/modules/bookings/bookings.controller.ts
+++ src/modules/bookings/bookings.controller.ts
@@ -3,6 +3,8 @@ import type { Request,Response } from "express";
 import { createBookingSchema } from "./bookings.validation";
 import { createBooking } from "./bookings.service";
 import { sendResponse } from "../../utils/send-response";
+import z from "zod";
+import prisma from "../../lib/prisma";
 export const addBooking=catchAsync(async(req:Request,res:Response)=>{
 const input=createBookingSchema.parse(req.body)
 const booking=await createBooking(req.user!.id,input)
@@ -10,3 +12,30 @@ const booking=await createBooking(req.user!.id,input)
 sendResponse(res,{message:"Booking Successfully",data:{booking}},201)
 
 })
+
+
+export const getAllBookings=catchAsync(async(req:Request,res:Response)=>{
+const equipment=await prisma.booking.findMany({
+    where:{
+        status:"CONFIRMED"
+    },
+    orderBy:{
+        createdAt:"desc"
+    }
+})
+sendResponse(res,{message:"Equipments retrived successfully",data:{equipment}})
+
+})
+
+const bookingIdSchema=z.object({
+    id:z.uuid()
+
+})
+// export const getMyBooking=catchAsync(async(req:Request,res:Response)=>{
+//    const {id}=bookingIdSchema.parse(req.params);
+
+//    console.log(id)
+//    const equipment= await getEquipmentById(id)
+//    sendResponse(res,{message:"Car retrived Successfully",data:{equipment}})
+//    return{} 
+// })
--- src/modules/bookings/bookings.route.ts
+++ src/modules/bookings/bookings.route.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import auth from "../../middleware/auth";
-import { addBooking } from "./bookings.controller";
+import { addBooking, getAllBookings } from "./bookings.controller";
 
 export const bookingRouter=Router()
 bookingRouter.post("/",auth("CUSTOMER"),addBooking)
+bookingRouter.get("/",auth("ADMIN"),getAllBookings)
