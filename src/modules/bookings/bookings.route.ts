77dd83cfead70587bee1e60dcb6b463e91d505f7
fhkathika <fhkathika057@gmail>
2026-08-22 11:41:15 +0600

    edited  get my bookings api

--- src/modules/bookings/bookings.route.ts
+++ src/modules/bookings/bookings.route.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import auth from "../../middleware/auth";
-import { addBooking, getAllBookings } from "./bookings.controller";
+import { addBooking, getAllBookings, getMyBooking } from "./bookings.controller";
 
 export const bookingRouter=Router()
 bookingRouter.post("/",auth("CUSTOMER"),addBooking)
 bookingRouter.get("/",auth("ADMIN"),getAllBookings)
+bookingRouter.get("/my",auth("CUSTOMER"),getMyBooking)
