903a6d0049c72becb035e5ebb1e40ebf3e2e165c
fhkathika <fhkathika057@gmail>
2026-08-22 13:19:28 +0600

    working on post payments api

--- src/modules/payment/payment.route.ts
+++ src/modules/payment/payment.route.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import auth from "../../middleware/auth";
+import { checkout } from "./payment.controller";
 
 const paymentRouter=Router()
 
-// paymentRouter.post("/checkout/:bookingId",auth("CUSTOMER"),checkout)
+paymentRouter.post("/checkout/:bookingId",auth("CUSTOMER"),checkout)
 
 export default paymentRouter
